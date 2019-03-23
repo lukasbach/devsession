6307d17891064007e883ecf13de91c1cba72afe6
Lukas Bach <lbach@outlook.de>
2019-03-23 23:24:09 +0100

    Reworked user cards

--- packages/frontend/src/components/UserList/UserCard.tsx
+++ packages/frontend/src/components/UserList/UserCard.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
 import {IUserWithLocalData} from "../../store/users";
 import {useState} from "react";
-import {SocketServer} from "../../utils/socket";
-import {SocketMessages} from "../../types/communication";
+import {AnchorButton, Button, ButtonGroup, Card, Classes, Elevation} from "@blueprintjs/core";
 
 export const UserCard: React.FunctionComponent<{
   user: IUserWithLocalData
@@ -10,17 +9,39 @@ export const UserCard: React.FunctionComponent<{
   const [editModel, setEditModel] = useState<IUserWithLocalData | null>(null);
 
   return (
-    <div>
-      <div
-        style={{ margin: '.2em' }}
-      >
-        <div>
-          <div>{ editModel ? 'Editing your profile' : props.user.name }</div>
-          <div>
-            {JSON.stringify(props.user)}
-          </div>
-        </div>
-      </div>
-    </div>
+    <Card
+      interactive={true}
+      elevation={Elevation.TWO}
+      style={{ borderLeft: `8px solid ${props.user.color}`, margin: '.5em' }}
+    >
+      <h5 className={Classes.HEADING}><a href="#">{ props.user.name }</a></h5>
+      <p className={[Classes.TEXT_SMALL, Classes.TEXT_MUTED, Classes.TEXT_OVERFLOW_ELLIPSIS].join(' ')}>
+        {
+          props.user.position.path
+            ? props.user.position.path
+              + (
+                props.user.position.cursor
+                  ? `:${props.user.position.cursor!.lineNumber}:${props.user.position.cursor!.column}`
+                  : ''
+              )
+            : 'No files open.'
+        }
+      </p>
+
+      {
+        props.user.isItMe
+          ? (
+            <ButtonGroup>
+              <Button icon={'edit'}>Edit my data</Button>
+            </ButtonGroup>
+          )
+          : (
+            <ButtonGroup>
+              <Button icon={'eye-open'}>Show in code</Button>
+              <AnchorButton rightIcon="caret-down">More</AnchorButton>
+            </ButtonGroup>
+          )
+      }
+    </Card>
   )
 };
