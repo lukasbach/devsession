f33c6d03091f16b8f4ab51cb0dbea2d68f2d9da8
Lukas Bach <lbach@outlook.de>
2019-04-21 23:25:04 +0200

    Slight changes to backend

--- packages/backend/src/cli.ts
+++ packages/backend/src/cli.ts
@@ -3,8 +3,8 @@ import {initApp} from "./init";
 import {IServerSettings} from "./ServerSettings";
 
 commander
-  .version("0.1.0")
-  .option("-p, --port [port]", "The port on which to run the server")
+  .version("0.0.0")
+  .option("-p, --port [port]", "The port on which to run the server. Defaults to 8020.")
   .option("-k, --adminkey [key]", "This key can be used to register a user as an admin. Defaults to a random string.")
   .option("-d, --dir [dir]", "The project directory. Defaults to the current directory.")
   .parse(process.argv);
