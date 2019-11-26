3032f2869de87256038e0a80967873101bd0f98a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-26 14:17:21 +0000

    Deploy lukasbach/devsession to github.com/lukasbach/devsession.git:gh-pages

---
+++ precache-manifest.e86fdd6b6492ef8a05c5ca9f47a74f82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "68b2b6dba44b7a879f08",
+    "url": "/static/js/main.b3980a4e.chunk.js"
+  },
+  {
+    "revision": "56576593bb0797822173",
+    "url": "/static/js/2.bceccb28.chunk.js"
+  },
+  {
+    "revision": "68b2b6dba44b7a879f08",
+    "url": "/static/css/main.60633eb1.chunk.css"
+  },
+  {
+    "revision": "43b86eaf293912e4ff34cd8297f44d56",
+    "url": "/index.html"
+  }
+];
